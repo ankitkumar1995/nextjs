e6ab9434b9a2b8d4c8ee2026ab5c0096e57f3a1e
ankitkn1995 <ankitknit15@gmail.com>
2023-04-20 08:23:51 +0530

    internationalization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ["en", "kn"],
+    defaultLocale: "en",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
